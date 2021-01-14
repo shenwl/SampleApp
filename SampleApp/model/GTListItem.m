6fd0342cbe57ccd8a2d9d48011ada2df1d84a2a2
shenwl <shenweilun96@gmail.com>
2021-01-14 22:21:36 +0800

    model实现NSSecureCoding

--- SampleApp/model/GTListItem.m
+++ SampleApp/model/GTListItem.m
@@ -9,6 +9,38 @@
 
 @implementation GTListItem
 
+#pragma mark - NSSecureCoding
+
+-(nullable instancetype) initWithCoder: (NSCoder *)aDecoder {
+    self = [super init];
+    if(self) {
+        self.category = [aDecoder decodeObjectForKey:@"category"];
+        self.picUrl = [aDecoder decodeObjectForKey:@"thumbnail_pic_s"];
+        self.uniquekey = [aDecoder decodeObjectForKey:@"uniquekey"];
+        self.title = [aDecoder decodeObjectForKey:@"title"];
+        self.date = [aDecoder decodeObjectForKey:@"date"];
+        self.authorName = [aDecoder decodeObjectForKey:@"author_name"];
+        self.articleUrl = [aDecoder decodeObjectForKey:@"url"];
+    }
+    return self;
+}
+
+- (void) encodeWithCoder:(NSCoder *)aCoder {
+    [aCoder encodeObject:self.category forKey:@"category"];
+    [aCoder encodeObject:self.picUrl forKey:@"picUrl"];
+    [aCoder encodeObject:self.uniquekey forKey:@"uniquekey"];
+    [aCoder encodeObject:self.title forKey:@"title"];
+    [aCoder encodeObject:self.date forKey:@"date"];
+    [aCoder encodeObject:self.authorName forKey:@"authorName"];
+    [aCoder encodeObject:self.articleUrl forKey:@"articleUrl"];
+}
+
++ (BOOL) supportsSecureCoding {
+    return YES;
+}
+
+#pragma mark - public method
+
 -(void) configWithDictionary: (NSDictionary *) dictnary {
 #warning 类型是否匹配
     self.category = [dictnary objectForKey:@"category"];
